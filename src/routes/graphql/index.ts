b5cd5781087fc99fde557b4ea4ae7ab5fab649cc
Pavel Sheshko <psheshko@twelvedevs.com>
2023-07-24 12:24:08 +0300

    feat: configure .env and start add logic

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -2,6 +2,8 @@ import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
 import { graphql } from 'graphql';
 
+// temp
+
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
   fastify.route({
     url: '/',
